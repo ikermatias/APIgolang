89e125f5da69ee8cfca697ecf5dbc78924a3a96a
Sebastian garcia <josegar0218@gmail.com>
2019-06-10 20:41:49 -0500

    Update web

--- webServer.go
+++ webServer.go
@@ -553,6 +553,7 @@ func listAll(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			break
 		}
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 
 		w.Write([]byte(id))
